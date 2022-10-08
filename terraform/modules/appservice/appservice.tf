465ff8722b685bd049c4e9bfcfe9e101196bca9a
Usama <s249895@studenti.polito.it>
2022-10-09 01:53:34 +0200

    reverted appservice change

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,4 +1,4 @@
-resource "azurerm_service_plan" "test" {
+resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
@@ -13,7 +13,7 @@ resource "azurerm_app_service" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  app_service_plan_id = azurerm_service_plan.test.id
+  app_service_plan_id = azurerm_app_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
